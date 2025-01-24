fd5e5e46873552ede642962108945edb68d315d6
Flo301 <neuflo.weller@gmail.com>
2025-01-24 20:44:44 +0100

    - Itemslots - Bubble - DebugSprites

--- scripts/Bubble.cs
+++ scripts/Bubble.cs
@@ -1,18 +1,11 @@
 using Godot;
 using System;
 
-public partial class GameManager : Node
+public partial class Bubble : RigidBody2D
 {
-	static public GameManager Instance { get; private set; }
-
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		if (Instance != null)
-		{
-			throw new Exception("Singleton already instantiated");
-		}
-		Instance = this;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
