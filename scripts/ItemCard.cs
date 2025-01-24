b1d1fb229a6ffcd8cb704bde52f9c72f814c8bc2
rw <robinweller20@gmail.com>
2025-01-24 23:47:50 +0100

    moved BoxUI into its own folder created ItemCard and its corosponding Script

---
+++ scripts/ItemCard.cs
@@ -0,0 +1,48 @@
+using Godot;
+using System;
+
+public partial class ItemCard : Control
+{
+	[Export]
+	public Texture2D Icon;
+	private TextureRect IconRect;
+	
+	[Export(PropertyHint.Enum, "common,uncommon,rare,epic,legendary")]
+	public string Rarity;
+	
+	//[Export]
+	//public Texture2D CardBackground;
+	private NinePatchRect CardBackgroundRect;
+	
+	[Export]
+	public string NameText;
+	private Label NameLabel;
+	
+	[Export]
+	public string DescriptionText;
+	private Label DescriptionLabel;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		IconRect = GetNode<TextureRect>("%Icon");
+		CardBackgroundRect = GetNode<NinePatchRect>("%CardBackground");
+		NameLabel = GetNode<Label>("%NameLabel");
+		DescriptionLabel = GetNode<Label>("%DescriptionLabel");
+		
+		IconRect.Texture = Icon;
+		CardBackgroundRect.Texture = (Texture2D)GD.Load("res://sprites/BoxUI/BoxUI-" + Rarity + ".png");
+		NameLabel.Text = NameText;
+		DescriptionLabel.Text = DescriptionText;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	public void OnMouseEnter()
+	{
+		
+	}
+}
