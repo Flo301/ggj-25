c50bf879f83edaed557ff2e70603bde99c07ad60
rw <robinweller20@gmail.com>
2025-01-26 01:23:03 +0100

    added GridItemRarity instances to RandomItemPool, added ideas to ToDo, changed window scaling configuration

--- scripts/GameManager.cs
+++ scripts/GameManager.cs
@@ -35,6 +35,8 @@ public partial class GameManager : Node
 	private int Points = 0;
 	private int round = 0;
 	private int Round { get => round; set { RoundLabel.Text = "Round " + value; round = value; } }
+	private double Stage = 0;
+	private int StagePointsBase = 450;
 
 	public void StartNextRound()
 	{
@@ -49,13 +51,42 @@ public partial class GameManager : Node
 	{
 		GD.Print("OnRoundEnd");
 		IsRoundActive = false;
-		ItemManager.Instance.OnRoundEnd();
-		ItemManager.Instance.GetRandomItem();
+		if(Round % 5 != 0)
+		{
+			ItemManager.Instance.OnRoundEnd();
+			ItemManager.Instance.GetRandomItem();
+			return;
+		}
+		else if(GetCurrentStageGoal() <= Points)
+		{
+			Stage++;
+			//gerade nur ein workaround um die punkteanzeige zu aktualisieren
+			AddPoints(0);
+			
+			ItemManager.Instance.OnRoundEnd();
+			// hinzufügen das die anzahl an Items und Upgrades Variabel ist GetRandomItem => GetRandomCards(items: X, upgrades: Y)
+			ItemManager.Instance.GetRandomItem();
+			return;
+		}
+		GD.Print("LOST");
 	}
 
 	public void AddPoints(int amount)
 	{
 		Points += amount;
-		PointLabel.Text = Points + "P";
+		PointLabel.Text = Points + "P / " + GetCurrentStageGoal() + "P";
+	}
+	
+	public double GetCurrentStageGoal()
+	{
+		switch (Stage)
+		{
+			case >= 5:
+				return (StagePointsBase + (StagePointsBase * Stage) * Stage + (75 * Stage) * Stage );
+			case >= 2:
+				return (Math.Floor(StagePointsBase + (StagePointsBase * Stage) * (0.75 * Stage) + (75 * Stage) * (0.75 * Stage) ));
+			default:
+				return (StagePointsBase + (StagePointsBase * Stage) + (75 * Stage) );
+		}
 	}
 }
